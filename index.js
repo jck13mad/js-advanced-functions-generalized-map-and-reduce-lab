b14f9c808dca8a9c79fd7c744707465621fcfd53
John Madden <jck13mad@gmail.com>
2021-03-23 13:14:15 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+function map(sourceArray, callBackFn) {
+    const newArray = [];
+    for (let i = 0; i < sourceArray.length; i++) {
+        newArray.push(callBackFn(sourceArray[i]))
+    }
+    return newArray;
+}
+
+function reduce(sourceArray, callBackFn, startPoint) {
+    let total = 0;
+    let i = 0;
+
+    if (startPoint) {
+        total = startPoint;
+    } else {
+        total = sourceArray[0];
+        i++;
+    }
+
+    for (i; i < sourceArray.length; i++) {
+        total = callBackFn(sourceArray[i], total)
+    }
+    return total;
+}
